52c8ca104bea888ee60ace98b49dc739ffc78a05
balaji049 <049balaji@gmail.com>
2026-07-20 22:15:13 +0530

    update rag pipeline

--- python-ai/app/main.py
+++ python-ai/app/main.py
@@ -1,11 +1,45 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.api.health import router as health_router
+#from app.api.documents import router as document_router
+from app.api.chat import router as chat_router
+from app.api.test import router as test_router
 
-@app.get("/")
-def root():
-    return {"status": "ok"}
 
-@app.get("/api/health")
-def health():
-    return {"status": "healthy"}
+
+app = FastAPI(
+
+    title="Enterprise AI Service",
+
+    version="1.0.0"
+
+)
+
+
+
+app.include_router(
+    test_router,
+    prefix="/api"
+)
+
+#app.include_router(
+
+#    document_router,
+
+#    prefix="/api"
+
+#)
+
+app.include_router(
+    health_router,
+    prefix="/api"
+)
+
+app.include_router(
+
+    chat_router,
+
+    prefix="/api"
+
+)
+
