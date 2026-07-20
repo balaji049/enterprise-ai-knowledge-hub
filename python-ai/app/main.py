0f438077ee7148080b036638bffa045d7a5697fb
balaji049 <049balaji@gmail.com>
2026-07-20 22:24:57 +0530

    commit

--- python-ai/app/main.py
+++ python-ai/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 from app.api.health import router as health_router
 #from app.api.documents import router as document_router
-from app.api.chat import router as chat_router
+#from app.api.chat import router as chat_router
 from app.api.test import router as test_router
 
 
@@ -35,11 +35,11 @@ app.include_router(
     prefix="/api"
 )
 
-app.include_router(
+#app.include_router(
 
-    chat_router,
+#    chat_router,
 
-    prefix="/api"
+#    prefix="/api"
 
-)
+#)
 
